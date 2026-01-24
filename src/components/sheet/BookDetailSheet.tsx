f12cfd16b596e9835a7c56755fd8f2070e087100
Abduraxmon-st <2442abd72@gmail.com>
2026-01-24 18:32:53 +0500

    scrolling animation added

--- src/components/sheet/BookDetailSheet.tsx
+++ src/components/sheet/BookDetailSheet.tsx
@@ -11,6 +11,7 @@ export const BookDetailSheet = () => {
   const startY = useRef<number | null>(null);
   const isDragging = useRef(false);
   const [isAnimating, setIsAnimating] = useState(true);
+  const contentRef = useRef<HTMLDivElement | null>(null);
 
   // close outside
   useEffect(() => {
@@ -44,12 +45,13 @@ export const BookDetailSheet = () => {
     if (!isDragging.current || startY.current === null) return;
 
     const diff = y - startY.current;
+    const scrollTop = contentRef.current?.scrollTop ?? 0;
 
-    if (diff > 0) {
+    // 👇 faqat content tepasida bo‘lsa
+    if (scrollTop === 0 && diff > 0) {
       setOffsetY(diff);
     }
   };
-
 
   // End Touch 
   const handleEnd = () => {
@@ -62,7 +64,16 @@ export const BookDetailSheet = () => {
     isDragging.current = false;
     startY.current = null;
   };
+  // sheet full open to scroll top
+  const handleContentScroll = () => {
+    if (!contentRef.current) return;
 
+    if (contentRef.current.scrollTop > 0) {
+      setOffsetY(-72);
+    } else if (contentRef.current.scrollTop === 0) {
+      setOffsetY(0)
+    }
+  };
 
   if (bookDet) {
     document.body.style.overflow = 'hidden';
@@ -72,7 +83,7 @@ export const BookDetailSheet = () => {
   return (
     <div ref={wrapperRef}
       style={{ transform: `translate(0, ${offsetY}px)` }}
-      className={`fixed left-0 top-0 inset-0 bg-white text-mainColor z-50 p-4 pt-0! ${!!bookId ? 'translate-y-18' : 'translate-y-full'} ${isAnimating ? "transition-transform duration-300 ease-out" : ""} ease-in-out rounded-4xl shadow-[0px_-4px_31px_0px_#00000070]`}>
+      className={`fixed left-0 top-0 inset-0 bg-white text-mainColor z-50 p-4 pt-0! ${!!bookId ? 'translate-y-18 shadow-[0px_-4px_31px_0px_#00000070]' : 'translate-y-[110%] shadow-[0px_0px_0px_0px_#00000000]'} ${isAnimating ? "transition-transform duration-300 ease-out" : ""} rounded-4xl`}>
       {/* <button onClick={() => resetBookId()}
         className="p-1.5 bg-mainColor text-white rounded-full">
         <ArrowRightIcon className="size-7 rotate-180" />
@@ -84,8 +95,15 @@ export const BookDetailSheet = () => {
         className="py-4 flex justify-center">
         <div className="w-15 h-2 bg-descColor/50 rounded-full"></div>
       </div>
-      <img src={bookImage} alt={bookDet?.title} className="w-[80%] mx-auto mt-2 rounded-2xl" />
-      <div className="h-screen"></div>
+      <div
+        ref={contentRef}
+        onScroll={handleContentScroll}
+        style={{ scrollbarWidth: "none" }}
+        className="overflow-x-auto w-full h-full">
+
+        <img src={bookImage} alt={bookDet?.title} className="w-[80%] mx-auto mt-2 rounded-2xl" />
+        <div className="h-screen"></div>
+      </div>
     </div>
   )
 }
